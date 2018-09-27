4fab69d6649fa501766676b4b1c5b6f88e769008
Alex Beregszaszi <alex@rtfs.hu>
2018-09-27 21:28:04 +0100

    Add basic tests for validateAndMeter

--- test/DummySentinel.js
+++ test/DummySentinel.js
@@ -12,4 +12,10 @@ contract('DummySentinel', async (accounts) => {
      let costTable = await instance.getCostTable.call();
      assert.equal(costTable.valueOf(), "0xdEADbeEF00000000000000000000000000000001");
   })
+  it("validateAndMeter", async () => {
+     let instance = await DummySentinel.deployed();
+     let input = Buffer.from("0061736d001122334455", "hex");
+     let output = await instance.validateAndMeter.call(input);
+     assert.equal(input, output);
+  })
 })
